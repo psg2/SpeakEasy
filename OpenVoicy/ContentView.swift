5e222474e6adaa2f9bb65d262d997ea18a0e9f9d
Pedro Sereno <peu.sereno@gmail.com>
2026-01-17 13:50:57 -0300

    Initial Commit

---
+++ OpenVoicy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenVoicy
+//
+//  Created by Arado on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenVoicy/OpenVoicyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenVoicyApp.swift
+//  OpenVoicy
+//
+//  Created by Arado on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenVoicyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
